8a0c80c5816839b20127e7124b971042dad37ad6
anaisthibault <thibault.anais27@gmail.com>
2026-06-25 00:49:51 +0200

    Canva top 10 v2

--- script/script.js
+++ script/script.js
@@ -5,11 +5,33 @@ async function chargerTracks() {
 }
 
 function creerGraphique(tracks){
-    const genres = {};
-    tracks.forEach(t => genres[t.genre] = (genres[t.genre] || 0) + 1);
+    const artists = {};
+    tracks.forEach(t => artists[t.artists[0].name] = (artists[t.artists[0].name] || 0) + 1);
+    const tableau = Object.entries(artists) ;
+    const tri = tableau.sort( (a,b) => {
+        return b[1] - a[1] ;
+    })
+    const topten = tri.slice(0,10)
+    const personne = topten.map(nom => {
+        return nom[0]; })
+    const compteur = topten.map(compteur => {
+        return compteur[1]; })
     new Chart(document.getElementById("chart"), {
         type: "bar",
-        data: {labels: Object.keys(genres), datasets: [{data: Object.values(genres)}]}
-    });
+        data: {
+            labels: personne, 
+            datasets: [{
+                label: "Nombre d'écoutes",
+                data: compteur
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+        }
+});
 }
 
